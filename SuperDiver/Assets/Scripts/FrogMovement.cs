a1b791b9f77df15cafe1750d01a152e4cb3349cd
Kai Yi (David) Zhang <theyijimima@gmail.com>
2020-06-05 10:38:40 -0400

    fixed frog animation

--- SuperDiver/Assets/Scripts/FrogMovement.cs
+++ SuperDiver/Assets/Scripts/FrogMovement.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class FrogMovement : MonoBehaviour
 {
-    enum FrogState { IDLE, JUMPUP, JUMPDOWN };
-    public const float jumpTime = 4f;
-    float jumpTimer = jumpTime; // in seconds
+    public float jumpTime = 4f;
+    float jumpTimer = 0f; // in seconds
     public float jumpForce = 6f;
     Rigidbody2D rb;
     Vector3 localScale;
     Animator anime;
-    FrogState currentState = FrogState.IDLE;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +17,7 @@ public class FrogMovement : MonoBehaviour
         localScale = transform.localScale;
         rb = GetComponent<Rigidbody2D>();
         anime = GetComponent<Animator>();
+        jumpTimer = jumpTime;
     }
 
     // Update is called once per frame
@@ -31,44 +30,15 @@ public class FrogMovement : MonoBehaviour
             jumpTimer = jumpTime;
         }
 
+        anime.SetFloat("jumpVelocity", rb.velocity.y);
 
-        if(rb.velocity.y > 0.01)
-        {
-            currentState = FrogState.JUMPUP;
-            
-        }
-        else if(rb.velocity.y < -0.01)
-        {
-            currentState = FrogState.JUMPDOWN;
-        }
-        else
-        {
-            currentState = FrogState.IDLE;
-        }
 
-        setAnimator();
     }
 
     void frogJump()
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
 
-    }
-
-    void setAnimator()
-    {
-        if(currentState == FrogState.JUMPUP)
-        {
-            anime.SetInteger("state", (int)FrogState.JUMPUP);
-        }
-        else if (currentState == FrogState.JUMPDOWN)
-        {
-            anime.SetInteger("state", (int)FrogState.JUMPDOWN);
-        }
-        else
-        {
-            anime.SetInteger("state", (int)FrogState.IDLE);
-        }
     }
 
 }
