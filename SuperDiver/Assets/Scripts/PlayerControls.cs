22b22789b60b8c3d70f0e8e2e81cb8ac5e8ab80d
Kai Yi (David) Zhang <theyijimima@gmail.com>
2020-06-05 15:56:44 -0400

    fixed player hitox when switching direction

--- SuperDiver/Assets/Scripts/PlayerControls.cs
+++ SuperDiver/Assets/Scripts/PlayerControls.cs
@@ -20,6 +20,8 @@ public class PlayerControls : MonoBehaviour
     public Vector2 velocity;          // current velocity of the player
     public JumpState jumpState = JumpState.IDLE;
 
+    Vector3 localScale; // for changing direction
+
     Rigidbody2D rb;
     Animator animator;
     SpriteRenderer spriteRenderer;
@@ -28,6 +30,7 @@ public class PlayerControls : MonoBehaviour
 
     void Awake()
     {
+        localScale = transform.localScale;
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
@@ -57,14 +60,18 @@ public class PlayerControls : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             isFacingRight = true;
-            spriteRenderer.flipX = !isFacingRight;
+            localScale.x = 1;
+            transform.localScale = localScale;
+            //spriteRenderer.flipX = !isFacingRight;
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
             animator.SetBool("isRunning", true);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             isFacingRight = false;
-            spriteRenderer.flipX = !isFacingRight;
+            localScale.x = -1;
+            transform.localScale = localScale;
+            //spriteRenderer.flipX = !isFacingRight;
             rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
             animator.SetBool("isRunning", true);
         }
