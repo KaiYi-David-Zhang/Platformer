8ec2a08361200d3e04339f7dab8f09799df2efbd
Kai Yi (David) Zhang <theyijimima@gmail.com>
2020-06-05 15:05:06 -0400

    cleaned code, deleted extra files, updated movement.
    Merged EagleMovement and OpossumMovement into EnemyHorizontalMovement.

--- SuperDiver/Assets/Scripts/EnemyHorizontalMovement.cs
+++ SuperDiver/Assets/Scripts/EnemyHorizontalMovement.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpossumMovement : MonoBehaviour
+public class EnemyHorizontalMovement : MonoBehaviour
 {
     public float leftDis, rightDis;
     public float moveSpeed = 3f;
     float startingPos;
     Vector3 localScale;
+    SpriteRenderer spriteRenderer;
     bool moveL = true;
     Rigidbody2D rb;
 
@@ -17,6 +18,7 @@ public class OpossumMovement : MonoBehaviour
     {
         localScale = transform.localScale;
         rb = GetComponent<Rigidbody2D> ();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         startingPos = transform.position.x;
     }
 
@@ -43,19 +45,16 @@ public class OpossumMovement : MonoBehaviour
     // normall the local should be 1 for facing right but the image is facing left to start with
     void moveRight() {
         moveL = false;
-        localScale.x = 1;
-        rb.velocity = new Vector2(localScale.x * moveSpeed, rb.velocity.y);
-        localScale.x = -1;  // here is the change since the picture is facing left
-        transform.localScale = localScale;
+        spriteRenderer.flipX = !moveL;
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
 
     }
 
 
     void moveLeft() {
         moveL = true;
-        localScale.x = -1;
-        rb.velocity = new Vector2(localScale.x * moveSpeed, rb.velocity.y);
-        localScale.x = 1;
-        transform.localScale = localScale;
+        spriteRenderer.flipX = !moveL;
+        rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+
     }
 }
