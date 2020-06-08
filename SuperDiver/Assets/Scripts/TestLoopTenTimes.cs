a594f14974ced4b1b8e056ad49a39c19d126a3e2
99inha <99inha.kim@gmail.com>
2020-06-08 15:57:21 -0400

    Cleaning up playaround mess

--- SuperDiver/Assets/Scripts/TestLoopTenTimes.cs
+++ SuperDiver/Assets/Scripts/TestLoopTenTimes.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/* This script is left alive solely for the purpose of referencing.
+ * In the "hurt" folder, a test animation has been implemented to adjust the animation
+ * when the player takes damage.
+ * Repeating 5 times with the animation rate set there seems pretty good.
+ * 
+ * See the rest of the script to see how animation can be repeated.
+ * Further note that an event calling "decrementAnimLooper" must be attached
+ * to the animation.
+ */
 public class TestLoopTenTimes : MonoBehaviour
 {
-    public int animLooper = 60;
+    public int animLooper = 5;
 
     Animator animator;
     // Start is called before the first frame update
