dc6d241ff542a08e17719ac3959091477a5dbc78
Kai Yi (David) Zhang <theyijimima@gmail.com>
2020-06-10 15:19:32 -0400

    created transition and script needed for transition

--- SuperDiver/Assets/Scripts/SceneChange.cs
+++ SuperDiver/Assets/Scripts/SceneChange.cs
@@ -6,13 +6,25 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     [SerializeField] private string sceneName;
-
+    public Animator animator;
+    
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.tag == "Player")
-        {
-            SceneManager.LoadScene(sceneName);
+        {   // Player hits the change scene place
+            // TODO: disable player's movement
+            StartCoroutine(loadNextLevel());
         }
+    }
+
+    IEnumerator loadNextLevel()
+    {
+        animator.SetTrigger("startTransition");
+
+        yield return new WaitForSeconds(1); // wait for 1 sceond
+
+        SceneManager.LoadScene(sceneName);
+
     }
 
 }
